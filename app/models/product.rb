e03e8ac282ac1caf07cbd6b57fc36d2930ec7e75
Trace Tessier <tessiert@unm.edu>
2019-01-03 17:22:51 -0700

    Added Cart and Line Item scaffolding

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+    has_many :line_items
+
+    before_destroy :ensure_not_referenced_by_any_line_item
+
     validates :title, :description, :image_url, presence: true
     validates :title, length: { minimum: 10 }
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -6,4 +10,11 @@ class Product < ApplicationRecord
     validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i, # ignore case
                                                        message: 'must be a URL 
                                                        for GIF, JPG, or PNG image.' }
+    def ensure_not_referenced_by_any_line_item
+        unless line_items.empty?
+            errors.add(:base, 'Line Items present')
+            throw :abort
+        end
+    end
+
 end
